ad9a040e108b4bf79c87f47a478f24bc42477f1c
scarhost <137666755+scarhost@users.noreply.github.com>
2024-11-03 21:39:43 -0800

    commit error

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const geistSans = localFont({
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "/images.png",
+  src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
